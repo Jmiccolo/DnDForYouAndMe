a1870f2244556612ee2eafaf5658a1bd7068fe06
Josh Miccolo <jmiccolo89@gmail.com>
2020-07-04 14:37:30 -0400

    Google Auth Setup

--- app.js
+++ app.js
@@ -94,6 +94,10 @@ app.use("/:UserId", userRoutes);
 app.use("/campaigns/:CampaignId/story", storyRoutes);
 app.use("/campaigns/:CampaignId/weapons", weaponRoutes);
 
+app.get("/DNDAPI/testpage", function(req, res){
+	res.render("DNDAPI/testpage")
+})
+
 
 app.listen(process.env.PORT||3000, process.env.IP, function(){
 	console.log("Server Has Started!");
